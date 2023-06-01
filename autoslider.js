f1403c50d50102b35bfcdd139f058a5b811a750d
aditbagja <aditbagja44@gmail.com>
2023-06-01 12:57:05 +0700

    fix delay slider & responsive mobile

--- autoslider.js
+++ autoslider.js
@@ -3,17 +3,21 @@ const wrapper = document.querySelector('.wrapper');
 const card = document.querySelector('.card');
 
 // Mengatur variabel untuk mengatur kecepatan autoplay dan arah scroll
-const autoplaySpeed = 2000; // Kecepatan autoplay dalam milidetik
+const autoplaySpeed = 6000; // Kecepatan autoplay dalam milidetik
 let scrollDirection = 'right'; // Arah scroll awal, dalam contoh ini ke kanan
 
 // Fungsi untuk menggerakkan slider
 function autoScroll() {
   if (scrollDirection === 'right') {
-    // Scroll ke kanan
-    wrapper.scrollLeft += card.offsetWidth;
+    // Scroll ke kanan jika belum mencapai akhir card terlihat
+    if (wrapper.scrollLeft + wrapper.offsetWidth < wrapper.scrollWidth) {
+      wrapper.scrollLeft += card.offsetWidth;
+    }
   } else {
-    // Scroll ke kiri
-    wrapper.scrollLeft -= card.offsetWidth;
+    // Scroll ke kiri jika belum mencapai awal card terlihat
+    if (wrapper.scrollLeft > 0) {
+      wrapper.scrollLeft -= card.offsetWidth;
+    }
   }
 }
 
@@ -31,6 +35,7 @@ function handleScrollBoundary() {
     }
   }
 }
+
 
 // Fungsi untuk memulai autoplay
 function startAutoplay() {
@@ -61,17 +66,22 @@ const wrapperBlack = document.querySelector('.wrapper-black');
 const cardBlack = document.querySelector('.card-black');
 
 // Mengatur variabel untuk mengatur kecepatan autoplay dan arah scroll
-const autoplaySpeedBlack = 3000; // Kecepatan autoplay dalam milidetik
+const autoplaySpeedBlack = 6000; // Kecepatan autoplay dalam milidetik
 let scrollDirectionBlack = 'right'; // Arah scroll awal, dalam contoh ini ke kanan
+let isScrolling = false; // Menyimpan status scrolling
 
 // Fungsi untuk menggerakkan slider
 function autoScrollBlack() {
   if (scrollDirectionBlack === 'right') {
-    // Scroll ke kanan
-    wrapperBlack.scrollLeft += cardBlack.offsetWidth;
+    // Scroll ke kanan jika belum mencapai akhir card terlihat
+    if (wrapperBlack.scrollLeft + wrapperBlack.offsetWidth < wrapperBlack.scrollWidth) {
+      wrapperBlack.scrollLeft += cardBlack.offsetWidth;
+    }
   } else {
-    // Scroll ke kiri
-    wrapperBlack.scrollLeft -= cardBlack.offsetWidth;
+    // Scroll ke kiri jika belum mencapai awal card terlihat
+    if (wrapperBlack.scrollLeft > 0) {
+      wrapperBlack.scrollLeft -= cardBlack.offsetWidth;
+    }
   }
 }
 
@@ -89,6 +99,7 @@ function handleScrollBoundaryBlack() {
     }
   }
 }
+
 
 // Fungsi untuk memulai autoplay
 function startAutoplayBlack() {
